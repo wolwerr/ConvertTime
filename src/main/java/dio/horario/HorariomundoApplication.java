be6fb418881e37da8a7b8475f41fb888114a41ee
wolwerr <ricardo@dtmm.com.br>
2021-11-17 00:17:22 -0300

    Initial commit

---
+++ src/main/java/dio/horario/HorariomundoApplication.java
@@ -0,0 +1,13 @@
+package dio.horario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HorariomundoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HorariomundoApplication.class, args);
+	}
+
+}
